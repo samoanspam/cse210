c333bb681fd865b7457baaf50a92b141c73dfd34
samoanspam <fateia.patane@gmail.com>
2024-04-27 18:39:52 -0600

    nice

--- sandbox/sandbox_project/Program.cs
+++ sandbox/sandbox_project/Program.cs
@@ -4,9 +4,60 @@ public class Program
 {
     static void Main(string[] args)
     {
-        // This project is here for you to use as a "Sandbox" to play around
-        // with any code or ideas you have that do not directly apply to
-        // one of your projects.
+        var numbers = new int[3];
+
+        numbers[0] = 1;
+        numbers[1] = 2;
+        numbers[2] = 3;
+
+        var index = 0;
+
+        // this is shorthand for initializing an array
+        // var numbers = new[] {1, 2, 3};
+
+        var numbersarray = new List<int> { 1, 2, 3};
+
+        // This is longhand of the above line 
+        // var numbersarray = new List<int>();
+        // numbersarray.Add(1);
+        // numbersarray.Add(2);
+        // numbersarray.Add(3);
+
+        //How to look up and check a certain index.
+        var thisvalue = numbersarray[2];
+
+        //How to append things to the list.
+        numbersarray.Add(thisvalue);
+
+        //How to insert a certain value in a specific index.
+        numbersarray.Insert(index, 0);
+
+        //How to remove an item from a specific index.
+        numbersarray.Remove(index);
+
+        //How to count the size of the array, not the capacity.
+        var arraysize = numbersarray.Count;
+        Console.WriteLine(arraysize);
+
+        //How to check capacity of an array
+        var arraycapacity = numbersarray.Capacity;
+        Console.WriteLine(arraycapacity);
+
+        //Why I would check if the array length is zero? Idk but here's how.
+        bool isempty = numbersarray.Count == 0;
+        Console.WriteLine(isempty);
+
+        //Now to access everything in the list using the foreach loop
+        foreach(var item in numbersarray)
+        {
+            Console.WriteLine(item);
+        }
+
+
+        Console.WriteLine(thisvalue);
+
+
+        System.Console.WriteLine("This is cool");
 
         Console.WriteLine("Hello Sandbox World!");
     }
--- week01/teach/Divisors.cs
+++ week01/teach/Divisors.cs
@@ -3,10 +3,21 @@ public static class Divisors {
     /// Entry point for the Divisors class
     /// </summary>
     public static void Run() {
+        Random random = new Random();
+
         List<int> list = FindDivisors(80);
         Console.WriteLine("<List>{" + string.Join(", ", list) + "}"); // <List>{1, 2, 4, 5, 8, 10, 16, 20, 40}
         List<int> list1 = FindDivisors(79);
         Console.WriteLine("<List>{" + string.Join(", ", list1) + "}"); // <List>{1}
+
+        var randomNumber = random.Next(1, 101);    
+        List<int> list2 = FindDivisors(randomNumber);
+    
+        Console.WriteLine($"The list for the random number {randomNumber} is:");    
+        Console.WriteLine("{" + string.Join(", ", list2) + "}");
+
+
+    
     }
 
     /// <summary>
@@ -18,7 +29,14 @@ public static class Divisors {
     /// <returns>List of divisors</returns>
     private static List<int> FindDivisors(int number) {
         List<int> results = new List<int>();
-        // Todo problem 1
+        
+        for (int i = 1; i <= number; i++)
+        {
+            if (number % i == 0)
+            {
+                results.Add(i);
+            }
+        }
         return results;
     }
 }
